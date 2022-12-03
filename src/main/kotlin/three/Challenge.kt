33ef0d4e026cfe6843da738508f0d7c72448e3b7
Ilker Uzun <ilker.uzun@sainsburys.co.uk>
2022-12-03 23:46:26 +0000

    Day 2 & 3

---
+++ src/main/kotlin/three/Challenge.kt
@@ -0,0 +1,32 @@
+package three
+
+import java.io.File
+
+fun main() {
+    val lines = File("/Users/ilker.uzun/git/advent/day-two/src/main/kotlin/three/input.txt")
+        .readLines()
+
+    println(part1(lines))
+    println(part2(lines))
+}
+
+private fun part1(lines: List<String>) = lines
+    .map {
+        Pair(
+            it.subSequence(0 until it.length / 2).toSet(),
+            it.subSequence(it.length / 2 until it.length).toSet()
+        )
+    }
+    .flatMap { pair -> pair.first.filter { pair.second.contains(it) } }
+    .sumOf { if (it >= 'a') it - 'a' + 1 else it - 'A' + 1 + 26 }
+
+
+private fun part2(lines: List<String>) = (lines.indices step 3)
+    .asSequence()
+    .flatMap { i ->
+        lines[i].asSequence().toSet()
+            .filter { lines[i + 1].contains(it) }
+            .filter { lines[i + 2].contains(it) }
+    }
+    .sumOf { if (it >= 'a') it - 'a' + 1 else it - 'A' + 1 + 26 }
+
---
+++ src/main/kotlin/two/Challenge.kt
@@ -0,0 +1,46 @@
+package two
+
+import java.io.File
+
+fun main() {
+    // A, X: Rock
+    // B, Y: Paper
+    // C, Z: Scissors
+
+    val moveMap = mapOf(Pair('A', 'X'), Pair('B', 'Y'), Pair('C', 'Z'))
+    val scoreMap = mapOf(Pair('X', 1), Pair('Y', 2), Pair('Z', 3))
+
+    val loseMap = mapOf(Pair('A', 'Z'), Pair('B', 'X'), Pair('C', 'Y'))
+    val drawMap = mapOf(Pair('A', 'X'), Pair('B', 'Y'), Pair('C', 'Z'))
+    val winMap = mapOf(Pair('A', 'Y'), Pair('B', 'Z'), Pair('C', 'X'))
+
+    val result = File("/Users/ilker.uzun/git/advent/day-two/src/main/kotlin/two/input.txt")
+        .readLines()
+        .filter { it.isNotEmpty() }
+        .map {
+            val first = it[0]
+            when (it[2]) {
+                'X' -> "$first ${loseMap[first]}"
+                'Y' -> "$first ${drawMap[first]}"
+                'Z' -> "$first ${winMap[first]}"
+                else -> {
+                    ""
+                }
+            }
+        }
+        .filter { it.isNotEmpty() }
+        .sumOf {
+            val self = it[2]
+            val opponent = moveMap[it[0]]!!
+
+            val diff = self - opponent
+            val draw = diff == 0
+            val win = diff == 1 || diff == -2
+
+            var score = if (draw) 3 else (if (win) 6 else 0)
+            score += scoreMap[self]!!
+            score
+        }
+
+    println("Result is $result")
+}
